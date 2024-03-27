497e76ca59b8dd66615c0114dce80f9e6d551a45
MathissGit <mathissineux@gmail.com>
2024-03-27 12:06:53 +0100

    modif les liens href

--- includes/partials/header.php
+++ includes/partials/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../style/header.css">
-	<script src="../src/hotel.js"></script>
-    <title>hotel</title>
+	<link rel="stylesheet" href="../../includes/style/header.css">
+	<script src="../../includes/src/hotel.js"></script>
+    <title>HOTEL</title>
 </head>
