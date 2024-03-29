3df85f198c056675e9042a6f2ee9d9275d081914
MathissGit <mathissineux@gmail.com>
2024-03-29 15:46:14 +0100

    css page de connexion

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php require_once('includes/partials/header.php') ?>
 
-<div class="container">
+<div class="container-form">
     <div class="login-form">
         <h2>Connexion</h2>
         <!-- TO DO : Rediriger le formulaire vers la page de gestion des utilisateur -->
@@ -14,6 +14,7 @@
             <input type="password" class="form-control" id="password" placeholder="Entrez votre mot de passe">
         </div>
         <button type="submit" class="btn btn-primary btn-block">Se connecter</button>
+        <a href="inscription.php">inscription</a>
         </form>
     </div>
 </div>
--- inscription.php
+++ inscription.php
@@ -1,27 +1,26 @@
 <?php require_once('includes/partials/header.php') ?>
-
   
-<div class="container">
+<div class="container-form">
     <div class="register-form">
-      <h2>Inscription</h2>
-      <!-- TO DO : rediriger vers les fonction qui inscrive un utilisateur dans la base de donnée -->
-      <form method="post">
-        <div class="form-group">
-          <label for="email">Email :</label>
-          <input type="email" class="form-control" id="email" placeholder="Entrez votre email">
-        </div>
-        <div class="form-group">
-          <label for="name">Nom :</label>
-          <input type="text" class="form-control" id="name" placeholder="Entrez votre nom">
-        </div>
-        <div class="form-group">
-          <label for="password">Mot de passe :</label>
-          <input type="password" class="form-control" id="password" placeholder="Entrez votre mot de passe">
-        </div>
-        <button type="submit" class="btn btn-primary btn-block">S'inscrire</button>
-      </form>
+        <h2>Inscription</h2>
+        <!-- TO DO : rediriger vers les fonction qui inscrive un utilisateur dans la base de donnée -->
+        <form method="post">
+            <div class="form-group">
+                <label for="name">Nom :</label>
+                <input type="text" class="form-control" id="name" placeholder="Entrez votre nom">
+            </div>
+            <div class="form-group">
+                <label for="email">Email :</label>
+                <input type="email" class="form-control" id="email" placeholder="Entrez votre email">
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe :</label>
+                <input type="password" class="form-control" id="password" placeholder="Entrez votre mot de passe">
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">S'inscrire</button>
+            <a href="connexion.php">Se Connecter</a>
+        </form>
     </div>
 </div>
-
 
 <?php require_once('includes/partials/footer.php') ?>
