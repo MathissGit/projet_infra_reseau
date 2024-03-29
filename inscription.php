5f142484f5ac1df96d5eb047fd75558a91d8e0cc
MathissGit <mathissineux@gmail.com>
2024-03-29 15:08:30 +0100

    ajout page d'inscription

---
+++ inscription.php
@@ -0,0 +1,27 @@
+<?php require_once('includes/partials/header.php') ?>
+
+  
+<div class="container">
+    <div class="register-form">
+      <h2>Inscription</h2>
+      <!-- TO DO : rediriger vers les fonction qui inscrive un utilisateur dans la base de donnée -->
+      <form method="post">
+        <div class="form-group">
+          <label for="email">Email :</label>
+          <input type="email" class="form-control" id="email" placeholder="Entrez votre email">
+        </div>
+        <div class="form-group">
+          <label for="name">Nom :</label>
+          <input type="text" class="form-control" id="name" placeholder="Entrez votre nom">
+        </div>
+        <div class="form-group">
+          <label for="password">Mot de passe :</label>
+          <input type="password" class="form-control" id="password" placeholder="Entrez votre mot de passe">
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">S'inscrire</button>
+      </form>
+    </div>
+</div>
+
+
+<?php require_once('includes/partials/footer.php') ?>
