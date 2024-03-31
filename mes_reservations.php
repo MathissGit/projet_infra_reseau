01517ebed54692bff825ba2b7ae78e018fb581eb
MathissGit <mathissineux@gmail.com>
2024-03-31 19:12:39 +0200

    création navbar et integration dans header.php

---
+++ mes_reservations.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+$PAGENAME = "HOTEL Acceuil";
+
+// Includes & Requires 
+require_once('includes/partials/header.php');
+require('serveur_db.php');
+
+?>
+
+
+
+
+
+<?php include('includes/partials/footer.php') ?>
