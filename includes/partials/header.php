659b00683204d8a99eaaf55f2a0e8f7881b007b8
MathissGit <mathissineux@gmail.com>
2024-04-09 22:52:09 +0200

    suppression link header

--- includes/partials/header.php
+++ includes/partials/header.php
@@ -11,7 +11,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../includes/style/style.css">
-	<script src="../../includes/src/hotel.js"></script>
     <script src="https://kit.fontawesome.com/dc65c3d7d4.js" crossorigin="anonymous"></script>
 </head>
 <body>
